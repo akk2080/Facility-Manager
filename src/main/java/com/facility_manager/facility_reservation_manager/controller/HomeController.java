bc2bc626f1f353d0986fd1b7f6a89166e4b15a3e
AAKARSH <akk2080@gmail.com>
2025-05-18 20:12:52 +0530

    create reservation modal added

--- src/main/java/com/facility_manager/facility_reservation_manager/controller/HomeController.java
+++ src/main/java/com/facility_manager/facility_reservation_manager/controller/HomeController.java
@@ -1,18 +1,29 @@
 package com.facility_manager.facility_reservation_manager.controller;
 
+import com.facility_manager.facility_reservation_manager.model.Reservation;
 import com.facility_manager.facility_reservation_manager.model.User;
+import com.facility_manager.facility_reservation_manager.service.ReservationService;
 import com.facility_manager.facility_reservation_manager.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.Set;
 
 @Controller
 public class HomeController {
 
     final UserService userService;
 
-    public HomeController(UserService userService) {
+    final ReservationService reservationService;
+
+    public HomeController(UserService userService, ReservationService reservationService) {
         this.userService = userService;
+        this.reservationService = reservationService;
     }
 
     @GetMapping("/")
@@ -21,10 +32,28 @@ public class HomeController {
     }
 
     @GetMapping("/reservations")
-    public String reservations(Model model) {
+    public String reservations(Model model, HttpSession session) {
         User user = userService.get(10000L);
-        model.addAttribute("user", user);
+        session.setAttribute("user", user);
+        Reservation reservation = new Reservation();
+        model.addAttribute("reservation", reservation);
 
         return "reservations";
     }
+
+    @PostMapping("/reservations-submit")
+    public String reservationsSubmit(@ModelAttribute Reservation reservation,
+                                     @SessionAttribute("user") User user) {
+
+        // Save to DB after updating
+        assert user != null;
+        reservation.setUser(user);
+        reservationService.create(reservation);
+        Set<Reservation> userReservations = user.getReservations();
+        userReservations.add(reservation);
+        user.setReservations(userReservations);
+        userService.update(user.getId(), user);
+        return "redirect:/reservations";
+    }
+
 }
